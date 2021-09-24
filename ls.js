420fb630de66851ad30d5a6b0e5d0c4664bbd2ed
LastDekay <77251869+LastDekay@users.noreply.github.com>
2021-09-24 16:30:08 -0400

    got it to workgit add .

--- ls.js
+++ ls.js
@@ -1,13 +1,13 @@
 
 const fs = require('fs');
 
-const runLs = fs.readdir('./','utf8',(err,files) => {
-    if(err){
-        throw err
-    } else {
-        process.stdout.write(files.join('\n'));
-        process.stdout.write("\nprompt > ");
-    }
-})
-
-module.exports = runLs;
+module.exports = function getLs(){
+    fs.readdir('./','utf8',(err,files) => {
+        if(err){
+            throw err
+        } else {
+            process.stdout.write(files.join('\n'));
+            process.stdout.write("\n\nprompt > ");
+        }
+    })
+}
--- pwd.js
+++ pwd.js
@@ -1,26 +1,4 @@
-const pwdVar = process.stdin.on('data', (data) => {
-
-  const cmd = data.toString().trim();
-  
-
-  if (cmd === 'pwd'){
-
-    process.stdout.write(process.cwd());
-
-  }
-
-  else if(cmd === 'ls'){
-    require('./ls');
-  }
-  
-  else {
-
-    process.stdout.write('You typed: ' + cmd);
-  }
-
-  process.stdout.write('\nprompt >');
-
-});
-module.exports = pwdVar;
-
+module.exports = function printPwd(){
+  process.stdout.write(process.cwd());
+} 
 
