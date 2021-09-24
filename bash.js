f9695067f5ec9f589a3ccf708ec99ee322cb28c3
LastDekay <77251869+LastDekay@users.noreply.github.com>
2021-09-24 15:33:13 -0400

    added ls, bug: non-repeating ls command

--- bash.js
+++ bash.js
@@ -1,3 +1,5 @@
 process.stdout.write('prompt >');
 
 require('./pwd');
+
+
--- pwd.js
+++ pwd.js
@@ -1,20 +1,26 @@
 const pwdVar = process.stdin.on('data', (data) => {
 
   const cmd = data.toString().trim();
-
+  
 
   if (cmd === 'pwd'){
 
-      process.stdout.write(process.cwd());
+    process.stdout.write(process.cwd());
 
-  } else {
+  }
 
-      process.stdout.write('You typed: ' + cmd);
+  else if(cmd === 'ls'){
+    require('./ls');
+  }
+  
+  else {
+
+    process.stdout.write('You typed: ' + cmd);
   }
 
   process.stdout.write('\nprompt >');
 
 });
-module.export = pwdVar;
+module.exports = pwdVar;
 
 
